5e5c9018be29f1b9c4631536f52c8fd69f1eeaa9
damianIracki <damian.iracki13@gmail.com>
2020-11-21 15:22:29 +0100

    added entity user, added new plugins, added events on buttons

--- app/src/main/java/com/p/homeapp/MainActivity.java
+++ app/src/main/java/com/p/homeapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.p.homeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.p.homeapp.entities.User;
+import com.p.homeapp.loginAndRegister.LoginActivity;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+        txtHome = findViewById(R.id.txtHome);
+        txtHome.setOnClickListener((v) ->{
+            Intent LoginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(LoginIntent);
+        });
     }
 }

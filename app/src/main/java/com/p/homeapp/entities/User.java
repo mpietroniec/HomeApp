a771c92a627f1a62a64548ea8d880f4569cc985f
damianIracki <damian.iracki13@gmail.com>
2020-11-23 22:46:47 +0100

    added registration feature

--- app/src/main/java/com/p/homeapp/entities/User.java
+++ app/src/main/java/com/p/homeapp/entities/User.java
@@ -1,6 +1,6 @@
 package com.p.homeapp.entities;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,15 +17,15 @@ public class User {
     private String login;
     private String email;
     private String password;
-    private Timestamp createDate;
+    private LocalDateTime createDate;
     private String role;
 
-    public User(long id, String login, String email, String password, Timestamp createDate) {
+    public User(long id, String login, String email, String password, LocalDateTime createDate) {
         this.id = id;
         this.login = login;
         this.email = email;
         this.password = password;
-        this.createDate = createDate;
+        this.createDate = LocalDateTime.now();
         this.role = "ROLE_USER";
     }
 }

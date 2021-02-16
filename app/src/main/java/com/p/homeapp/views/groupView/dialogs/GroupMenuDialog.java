687f1b0f90f3d19b89b5b60631a1d33b331f02da
damianIracki <damian.iracki13@gmail.com>
2021-02-16 20:18:17 +0100

    fixed invitation bug

--- app/src/main/java/com/p/homeapp/views/groupView/dialogs/GroupMenuDialog.java
+++ app/src/main/java/com/p/homeapp/views/groupView/dialogs/GroupMenuDialog.java
@@ -15,12 +15,14 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.p.homeapp.R;
 import com.p.homeapp.entities.Group;
@@ -43,6 +45,7 @@ public class GroupMenuDialog extends AppCompatDialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
@@ -57,89 +60,81 @@ public class GroupMenuDialog extends AppCompatDialogFragment {
                     }
                 })
                 .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         usernameToFind = eTxtFindUsername.getText().toString();
-                        sendInvite(usernameToFind);
+                        checkUserExist();
+                        //sendInvite(usernameToFind);
                     }
                 });
         return builder.create();
     }
 
-    private void sendInvite(String usernameToFind) {
 
-        FirebaseDatabase.getInstance().getReference().child("users").addValueEventListener(new ValueEventListener() {
+    private void checkUserExist() {
+        FirebaseDatabase.getInstance().getReference().child("users").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                boolean flag = true;
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                    User user = dataSnapshot.getValue(User.class);
-                    if (user.getLogin().toLowerCase().equals(usernameToFind.toLowerCase())) {
-                        userToInvite = dataSnapshot.getValue(User.class);
-                        checkUserIsAlreadyInvited();
-                        flag = false;
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    boolean flag = false;
+                    DataSnapshot snapshot = task.getResult();
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        User user = dataSnapshot.getValue(User.class);
+                        if (user.getLogin().toLowerCase().equals(usernameToFind.toLowerCase())) {
+                            flag = true;
+                            userToInvite = dataSnapshot.getValue(User.class);
+                            checkUserIsAlreadyGroupMember();
+                        }
+                    }
+                    if (flag == false) {
+                        Toast.makeText(mContext, "User doesn't exist", Toast.LENGTH_SHORT).show();
                     }
                 }
-                if (flag) {
-                    Toast.makeText(mContext, "User does't exist", Toast.LENGTH_SHORT).show();
-                }
             }
+        });
+    }
 
+    private void checkUserIsAlreadyGroupMember(){
+        FirebaseDatabase.getInstance().getReference().child("groups").child(groupId).get()
+                .addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if(task.isSuccessful()){
+                    Group group = task.getResult().getValue(Group.class);
+                    if(group.getMembersId().contains(userToInvite.getId())){
+                        Toast.makeText(mContext, "User is already group member", Toast.LENGTH_SHORT).show();
+                    } else {
+                        checkUserIsAlreadyInvited();
+                    }
+                }
             }
         });
     }
 
     private void checkUserIsAlreadyInvited() {
-
         FirebaseDatabase.getInstance().getReference().child("invitations").child(userToInvite.getId())
-                .addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        boolean flag = true;
+                .get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
 
-                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                            Invitation invitationForCheck = dataSnapshot.getValue(Invitation.class);
-                            if (invitationForCheck.getGroupId().equals(groupId)) {
-                                flag = false;
-                                Toast.makeText(mContext, "User is already invite", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        if (flag) {
-                            checkUserIsAlreadyGroupMember();
+                if (task.isSuccessful()) {
+                    boolean flag = false;
+                    DataSnapshot snapshot = task.getResult();
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        Invitation invitation = dataSnapshot.getValue(Invitation.class);
+                        if (invitation.getGroupId().equals(groupId)) {
+                            flag = true;
+                            Toast.makeText(mContext, "User is already invited", Toast.LENGTH_SHORT).show();
                         }
                     }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
+                    if (flag == false) {
+                        saveInvitation();
                     }
-                });
-
+                }
+            }
+        });
     }
-
-    private void checkUserIsAlreadyGroupMember() {
-        FirebaseDatabase.getInstance().getReference().child("groups").child(groupId)
-                .addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        Group group = snapshot.getValue(Group.class);
-                        if (group.getMembersId().contains(userToInvite.getId())) {
-                            Toast.makeText(mContext, "User is already group member", Toast.LENGTH_SHORT).show();
-                        } else {
-                            saveInvitation();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
-    }
-
 
     private void saveInvitation() {
 
@@ -152,7 +147,6 @@ public class GroupMenuDialog extends AppCompatDialogFragment {
         invitation.setId(invitationId);
         invitation.setGroupId(groupId);
         invitation.setInvitationSender(FirebaseAuth.getInstance().getCurrentUser().getUid());
-        invitation.setAccepted(false);
         reference.child(invitationId).setValue(invitation).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
